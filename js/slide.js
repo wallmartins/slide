5ab31936cefc7519c195ce0f263f0ed5f9e26776
Wallace <wall_martins_@hotmail.com>
2021-03-03 13:05:12 -0300

    navegacao prev e nav finalizada

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 import debounce from './debounce.js';
 
-export default class Slide {
+export class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper);
@@ -118,7 +118,7 @@ export default class Slide {
     }, 1000);
   }
 
-  addReziseEvent() {
+  addResizeEvent() {
     window.addEventListener('resize', this.onResize);
   }
 
@@ -126,6 +126,10 @@ export default class Slide {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
+
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
+
     this.onResize = debounce(this.onResize.bind(this), 200);
   }
 
@@ -134,7 +138,21 @@ export default class Slide {
     this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
-    this.addReziseEvent();
+    this.addResizeEvent();
+    this.changeSlide(0);
     return this;
+  }
+}
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener('click', this.activePrevSlide);
+    this.nextElement.addEventListener('click', this.activeNextSlide);
   }
 }
